bd77df6859791ec1124e0a9fa7e47e16bddce142
SamuelJLeonard <SamuelJLeonard@gmail.com>
2017-10-03 20:39:37 -0700

    Update two_sum_solution.py

--- two_sum/two_sum_solution.py
+++ two_sum/two_sum_solution.py
@@ -1,3 +1,10 @@
+### Original problem posed by leetcode.com:
+### Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+### You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+### I've extended the solution to return pairs of indices corresponding to all possible solutions.
+
+
 
 from itertools import combinations
 
